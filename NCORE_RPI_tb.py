43db235b73cae59669dd04373fc36adc4ee3ea77
Rocco Febbo <febbo87@gmail.com.com>
2024-06-03 16:43:35 -0400

    updated timing

--- NCORE_RPI_tb.py
+++ NCORE_RPI_tb.py
@@ -7,12 +7,16 @@ from micropython import const
 
 
 main_clk = False
+check_clk = False
 
 scan_data = '01010101'
 
 def flip_clk(t):
-    global main_clk
-    main_clk = True
+    global main_clk, check_clk
+    if check_clk == False:
+        main_clk = not main_clk
+        check_clk = True
+    
 
 
 if __name__ == '__main__':
@@ -39,22 +43,23 @@ if __name__ == '__main__':
     
     
     while run:
-        
-        if main_clk == False and prev_clk_state == True:
+        if check_clk:
+            check_clk = False
+            if main_clk == False and prev_clk_state == True:
+                
+                print(scan_data[current_bit])
             
-            print(scan_data[current_bit])
-        
-            if scan_data[current_bit] == '0':
-                p0_value = 0
-            elif scan_data[current_bit] == '1':
-                p0_value = 1
-            
-            current_bit += 1
-            
-            if current_bit >= len(scan_data):
-                run = False
-            
-            
+                if scan_data[current_bit] == '0':
+                    p0_value = 0
+                elif scan_data[current_bit] == '1':
+                    p0_value = 1
+                
+                current_bit += 1
+                
+                if current_bit >= len(scan_data):
+                    run = False
+                
+                
         p1.value(main_clk)
         p0.value(p0_value)
         prev_clk_state = main_clk
